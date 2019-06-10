0d0bf8075105d434dd04a6178033c942e217e2af
Dan Turkel <daturkel@gmail.com>
2019-06-09 21:18:09 -0400

    extra reqs

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description="An interface for associating objects with descriptive tags.",
     long_description=open("README.md").read(),
     install_requires=["boolean.py>=3.6", "ujson>=1.35"],
+    extras_require={"test": ["pytest>=4.6", "pytest-cov>=2.7", "coveralls>=1.8.0"]},
     url="https://github.com/daturkel/doctag",
     author="Dan Turkel",
     author_email="daturkel@gmail.com",
